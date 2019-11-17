c6371bc3354dcdc8624106a8739e80b568f03cc4
Myslawek <przemek.g96@wp.pl>
2019-11-17 22:04:57 +0100

    Add two model classes for whole Table and a single Cell.

---
+++ src/pl/polsl/largetableviewer/table/model/CellModel.java
@@ -0,0 +1,9 @@
+package pl.polsl.largetableviewer.table.model;
+
+public class CellModel {
+    private String cellContents;
+    private int row;
+    private int column;
+    private boolean isVisible;
+
+}
---
+++ src/pl/polsl/largetableviewer/table/model/TableModel.java
@@ -0,0 +1,11 @@
+package pl.polsl.largetableviewer.table.model;
+
+import java.util.List;
+
+public class TableModel {
+    private int numberOfColumns;
+    private int numberOfRows;
+    private List<CellModel> tableRows;
+
+
+}
