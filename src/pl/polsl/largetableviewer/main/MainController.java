573939f8b9f2390d00a39ec0865f81990b64eae6
Myslawek <przemek.g96@wp.pl>
2019-11-17 22:08:32 +0100

    Create a basic structure for a package

---
+++ src/pl/polsl/largetableviewer/main/MainController.java
@@ -0,0 +1,15 @@
+package pl.polsl.largetableviewer.main;
+
+import pl.polsl.largetableviewer.table.controller.TableController;
+
+/**
+ * Responsible for managing the communication between all controllers (JavaFX - GUI controller, Filter controller and Table controller).
+ */
+public class MainController {
+    //controllers
+    private TableController tableController = new TableController();
+
+    public static void main(String[] args){
+        //begin
+    }
+}
---
+++ src/pl/polsl/largetableviewer/table/controller/TableController.java
@@ -0,0 +1,6 @@
+package pl.polsl.largetableviewer.table.controller;
+
+public class TableController {
+
+
+}
