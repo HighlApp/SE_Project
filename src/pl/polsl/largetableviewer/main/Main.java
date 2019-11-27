724929bad5110efddec912a250293e48c4385b6d
Myslawek <przemek.g96@wp.pl>
2019-11-27 19:37:10 +0100

    Move the application starting point to another class

---
+++ src/pl/polsl/largetableviewer/main/Main.java
@@ -0,0 +1,9 @@
+package pl.polsl.largetableviewer.main;
+
+public class Main {
+    //Application EntryPoint
+    public static void main(String[] args){
+        MainController mainController = new MainController();
+        mainController.run();
+    }
+}
--- src/pl/polsl/largetableviewer/main/MainController.java
+++ src/pl/polsl/largetableviewer/main/MainController.java
@@ -1,15 +1,25 @@
 package pl.polsl.largetableviewer.main;
 
 import pl.polsl.largetableviewer.table.controller.TableController;
+import pl.polsl.largetableviewer.table.exception.TableControllerInitializationException;
 
 /**
  * Responsible for managing the communication between all controllers (JavaFX - GUI controller, Filter controller and Table controller).
  */
 public class MainController {
     //controllers
-    private TableController tableController = new TableController();
+    private TableController tableController;
 
-    public static void main(String[] args){
-        //begin
+
+    public void run(){
+        //Bartek controller
+        try {
+            tableController = new TableController(',', ';', "D:\\studia\\sem7\\SE\\SE_Project2\\doTestow\\firstSmallTable.txt");
+        }catch (TableControllerInitializationException ex){
+            ex.printStackTrace();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
+
 }
