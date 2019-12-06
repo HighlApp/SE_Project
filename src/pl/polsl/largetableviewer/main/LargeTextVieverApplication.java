18600e7c47e576694645405a446a38d57cd32814
Bartłomiej Góral <bartlomiej.goral@softiq.pl>
2019-12-06 03:32:15 +0100

    Layout improvements

--- src/pl/polsl/largetableviewer/main/LargeTextVieverApplication.java
+++ src/pl/polsl/largetableviewer/main/LargeTextVieverApplication.java
@@ -17,8 +17,9 @@ public class LargeTextVieverApplication extends Application {
         Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("Multiple Large Text File Viewer");
         setUserAgentStylesheet(STYLESHEET_MODENA);
-        primaryStage.setScene(new Scene(root, 1100, 750));
+        primaryStage.setScene(new Scene(root, 1100, 800));
         primaryStage.setMaxWidth(1100);
+        primaryStage.setMinWidth(800);
         primaryStage.show();
     }
 
